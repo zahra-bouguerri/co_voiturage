38c5c4e1192061841d0666326c83aa1229b47053
zahra-bouguerri <bouguerrifatmazohra@gmail.com>
2023-12-28 16:07:51 +0100

    modified:   ryztrips/contacter.php 	modified:   ryztrips/register.php 	deleted:    ryztrips/vendor/phpmailer/phpmailer 	renamed:    ryztrips/vendor/autoload.php -> vendor/autoload.php 	renamed:    ryztrips/vendor/composer/ClassLoader.php -> vendor/composer/ClassLoader.php 	renamed:    ryztrips/vendor/composer/InstalledVersions.php -> vendor/composer/InstalledVersions.php 	renamed:    ryztrips/vendor/composer/LICENSE -> vendor/composer/LICENSE 	renamed:    ryztrips/vendor/composer/autoload_classmap.php -> vendor/composer/autoload_classmap.php 	renamed:    ryztrips/vendor/composer/autoload_namespaces.php -> vendor/composer/autoload_namespaces.php 	renamed:    ryztrips/vendor/composer/autoload_psr4.php -> vendor/composer/autoload_psr4.php 	renamed:    ryztrips/vendor/composer/autoload_real.php -> vendor/composer/autoload_real.php 	renamed:    ryztrips/vendor/composer/autoload_static.php -> vendor/composer/autoload_static.php 	renamed:    ryztrips/vendor/composer/installed.json -> vendor/composer/installed.json 	renamed:    ryztrips/vendor/composer/installed.php -> vendor/composer/installed.php 	renamed:    ryztrips/vendor/composer/platform_check.php -> vendor/composer/platform_check.php 	new file:   vendor/phpmailer/phpmailer

--- ryztrips/register.php
+++ ryztrips/register.php
@@ -3,7 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require './vendor/autoload.php';
+require '../vendor/autoload.php';
 include('../config/connect.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
