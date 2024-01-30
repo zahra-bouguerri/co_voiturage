07aa88a8229d5fdebd527ad3f6c742e734790b88
randev-png <akrour.raniaa@gmail.com>
2024-01-30 18:21:18 +0100

    mise a jour et modification des fonctionnalites de la carte map et ajout l'ensemble des trajet existant a l'heure actuelle ainsi que le depart et l'arrivee au niveau de la page d'acceuil ains que l'inetface de l'administarteur

--- admin/settings.php
+++ admin/settings.php
@@ -50,7 +50,166 @@ include('../config/connect.php');
   </form>
     </div>
 </div>
+<div id="bingMap" style="position:relative;height:400px;"></div>
+<script type='text/javascript' src='https://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0&s=1'></script>
+<script type='text/javascript'>
+        var map, mapOptions; 
+        var pinInfobox = null;
 
+        function init() {
+            mapOptions = {
+                credentials: 'AoiyGy03-LqmZBoXyAArp6u-Rt9nobOLSDd5YoZ6lA1VD9IykV4v1F45td0Yw4Zs',
+                center: new Microsoft.Maps.Location(58.995311, -103.535156),
+                zoom: 3,
+                mapTypeId: Microsoft.Maps.MapTypeId.road
+            };
+
+            map = new Microsoft.Maps.Map(document.getElementById("bingMap"), mapOptions);
+            Microsoft.Maps.Events.addHandler(map, 'click', getLatlng);
+
+            // Request the user's location
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var loc = new Microsoft.Maps.Location(
+                    position.coords.latitude,
+                    position.coords.longitude);
+
+                // Add a pushpin at the user's location.
+                var pin = new Microsoft.Maps.Pushpin(loc);
+                map.entities.push(pin);
+
+                // Center the map on the user's location.
+                map.setView({ center: loc, zoom: 15 });
+            });
+            <?php
+$sql = "SELECT * FROM trajet";
+$result = $conn->query($sql);
+
+// Vérifier s'il y a des erreurs dans la requête SQL
+if (!$result) {
+    die("Erreur dans la requête SQL : " . $conn->error);
+}
+
+// Parcourir les résultats et afficher les trajets sur la carte
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "addTrajetToMap('" . $row['lieu_depart'] . "', '" . $row['destination'] . "');\n";
+    }
+} else {
+    echo "Aucun trajet trouvé dans la base de données.";
+}
+
+// Fermer la connexion à la base de données
+$conn->close();
+?>
+        }
+        
+        function addTrajetToMap(lieuDepart, destination) {
+    // Fonction pour géocoder une adresse et renvoyer les coordonnées
+    function geocodeAddress(address) {
+        return new Promise(function (resolve, reject) {
+            var geocodeRequest = "https://dev.virtualearth.net/REST/v1/Locations?query=" + address + "&key=" + mapOptions.credentials;
+
+            Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+                var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+
+                searchManager.geocode({
+                    where: address,
+                    callback: function (geocodeResult, userData) {
+                        if (geocodeResult && geocodeResult.results && geocodeResult.results.length > 0) {
+                            var location = geocodeResult.results[0].location;
+                            resolve(location);
+                        } else {
+                            reject("Erreur de géocodage pour l'adresse : " + address);
+                        }
+                    }
+                });
+            });
+        });
+    }
+
+    var lieuDepartCoord; // Déclarer lieuDepartCoord en dehors des promesses
+
+    // Géocoder le lieu de départ
+    geocodeAddress(lieuDepart)
+        .then(function (location) {
+            lieuDepartCoord = location; // Assigner la valeur à lieuDepartCoord
+
+            // Ajouter un pushpin pour le lieu de départ (départ)
+            var pushpinDepart = new Microsoft.Maps.Pushpin(lieuDepartCoord, {
+                color: 'green', // Couleur du pushpin pour le lieu de départ
+                title: 'Départ'
+            });
+
+            // Ajouter une infobox avec le nom du lieu de départ
+            pushpinDepart.metadata = {
+                title: 'Départ',
+                description: lieuDepart
+            };
+
+            Microsoft.Maps.Events.addHandler(pushpinDepart, 'click', displayInfobox);
+
+            map.entities.push(pushpinDepart);
+
+            // Géocoder la destination
+            return geocodeAddress(destination);
+        })
+        .then(function (destinationCoord) {
+            // Ajouter un pushpin pour la destination (arrivée)
+            var pushpinArrivee = new Microsoft.Maps.Pushpin(destinationCoord, {
+                color: 'red', // Couleur du pushpin pour la destination
+                title: 'Arrivée'
+            });
+
+            // Ajouter une infobox avec le nom du lieu d'arrivée
+            pushpinArrivee.metadata = {
+                title: 'Arrivée',
+                description: destination
+            };
+
+            Microsoft.Maps.Events.addHandler(pushpinArrivee, 'click', displayInfobox);
+
+            map.entities.push(pushpinArrivee);
+
+            // Créer une ligne entre le lieu de départ et la destination en rouge
+            var trajetLine = new Microsoft.Maps.Polyline([lieuDepartCoord, destinationCoord], {
+                strokeColor: 'blue',
+                strokeThickness: 2
+            });
+            map.entities.push(trajetLine);
+        })
+        .catch(function (error) {
+            alert(error);
+        });
+}
+
+// Fonction pour afficher l'infobox au clic sur le pushpin
+function displayInfobox(e) {
+    if (e.target.metadata) {
+        var infobox = new Microsoft.Maps.Infobox(e.target.getLocation(), {
+            title: e.target.metadata.title,
+            description: e.target.metadata.description,
+            visible: true
+        });
+        map.entities.push(infobox);
+    }
+}
+
+
+        function getLatlng(e) { 
+            if (e.targetType == "map") {
+                var point = new Microsoft.Maps.Point(e.getX(), e.getY());
+                var locTemp = e.target.tryPixelToLocation(point);
+                var location = new Microsoft.Maps.Location(locTemp.latitude, locTemp.longitude);
+
+                var pin = new Microsoft.Maps.Pushpin(location, {'draggable': false});
+
+                map.entities.push(pin);
+            }
+        }
+
+        // Call the init function when the page is loaded
+        window.onload = init;
+    </script>
 
 
 <?php include "./includes/footer.php" ?>
--- ryztrips/ajouter_trajet.php
+++ ryztrips/ajouter_trajet.php
@@ -14,32 +14,49 @@ if (isset($_POST['ajouter'])) {
     if (isset($_SESSION['userId'])) {
         $id_conducteur = $_SESSION['userId'];
     }
+    // Récupérer le nombre maximum de places depuis la table des paramètres (ajustez le nom de la table si nécessaire)
+    $queryMaxPlaces = "SELECT  nombre_max FROM paramètres  ";
+    $resultMaxPlaces = $conn->query($queryMaxPlaces);
 
-    // Convertir le format de date
-    $date = DateTime::createFromFormat('m/d/Y', $dateFormulaire);
-    $dateFormatee = $date->format('Y-m-d');
+    if ($resultMaxPlaces && $rowMaxPlaces = $resultMaxPlaces->fetch_assoc()) {
+        $nbMaxPlaces = $rowMaxPlaces ['nombre_max'];
 
-    // Convertir le format de l'heure
-    $heure = DateTime::createFromFormat('H:i', $heureFormulaire)->format('H:i:s');
+        // Vérifier si le nombre de places saisi est inférieur ou égal au maximum
+        if ($nb_places <= $nbMaxPlaces) {
+            // Convertir le format de date
+            $date = DateTime::createFromFormat('m/d/Y', $dateFormulaire);
+            $dateFormatee = $date->format('Y-m-d');
 
-    // Préparer la requête SQL d'insertion dans la table Trajet
-    $query = "INSERT INTO Trajet (lieu_depart, destination, nb_places_dispo, prix, date_trajet, heure_depart, id_conducteur) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            // Convertir le format de l'heure
+            $heure = DateTime::createFromFormat('H:i', $heureFormulaire)->format('H:i:s');
 
-    // Utiliser une déclaration préparée pour éviter les injections SQL
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("ssidssi", $depart, $destination, $nb_places, $prix, $dateFormatee, $heure, $id_conducteur);
+            // Préparer la requête SQL d'insertion dans la table Trajet
+            $query = "INSERT INTO Trajet (lieu_depart, destination, nb_places_dispo, prix, date_trajet, heure_depart, id_conducteur) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    // Exécuter la requête
-    if ($stmt->execute()) {
-       echo "<script>alert('Le trajet a été ajouté avec succès !'); window.location.href='conducteur.php?userId=" . $_SESSION['userId'] . "';</script>";
+            // Utiliser une déclaration préparée pour éviter les injections SQL
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("ssidssi", $depart, $destination, $nb_places, $prix, $dateFormatee, $heure, $id_conducteur);
+
+            // Exécuter la requête
+            if ($stmt->execute()) {
+                echo "<script>alert('Le trajet a été ajouté avec succès !'); window.location.href='conducteur.php?userId=" . $_SESSION['userId'] . "';</script>";
+            } else {
+                echo "<script>alert('Erreur lors de l\'ajout du trajet !'); window.location.href='conducteur.php?userId=" . $_SESSION['userId'] . "';</script>";
+            }
+
+            // Fermer la déclaration préparée
+            $stmt->close();
+        } else {
+            echo "<script>alert('Erreur: Le nombre de places ne doit pas dépasser $nbMaxPlaces   !! '); window.location.href='conducteur.php?userId=" . $_SESSION['userId'] . "';</script>";
+        }
     } else {
-       echo "<script>alert('Erreur lors de l\'ajout du trajet !'); window.location.href='conducteur.php?userId=" . $_SESSION['userId'] . "';</script>";
+        echo "<script>alert('Erreur lors de la récupération du nombre maximum de places !'); window.location.href='conducteur.php?userId=" . $_SESSION['userId'] . "';</script>";
     }
 
-    // Fermer la déclaration préparée
-    $stmt->close();
-}
+    // Fermer le résultat de la requête
+    $resultMaxPlaces->close();
 
-// Fermer la connexion à la base de données
-$conn->close();
+    // Fermer la connexion à la base de données
+    $conn->close();
+}
 ?>
